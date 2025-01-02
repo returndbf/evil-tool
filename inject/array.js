60cfd35023801d83d942a8be5a86b2b79f7c5dbc
returndbf <1668959325@qq.com>
2025-01-02 15:38:19 +0800

    comment: add func comment

--- inject/array.js
+++ inject/array.js
@@ -1,10 +1,13 @@
 import { injectCommon } from "./utils.js";
+// Intersection 交集
 injectCommon(Array, 'getIntersection', function (anotherArr) {
     return [...new Set(this.valueOf())].filter(item => new Set(anotherArr.valueOf()).has(item));
 })
+// Union 并集
 injectCommon(Array, 'getUnion', function (anotherArr) {
     return [...new Set([...this.valueOf(), ...anotherArr.valueOf()])]
 })
+// Difference 差集
 injectCommon(Array, 'getDifference', function (anotherArr) {
     return this.valueOf().filter(item => !anotherArr.valueOf().includes(item))
 })
--- inject/string.js
+++ inject/string.js
@@ -1,7 +1,9 @@
 import { injectCommon,parseStr2Number } from "./utils";
+// toFixed 保留小数
 injectCommon(String,'toFixed',function(digits = 2){
     return parseStr2Number(this.valueOf()).toFixed(digits)
 })
+// toLocaleString 特定于语言环境的表示字符串
 injectCommon(String,'toLocaleString',function(locales=undefined,options=undefined){
     return parseStr2Number(this.valueOf()).toLocaleString(locales,options)
 })
