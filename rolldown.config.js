5c324d6a8dac94103f3921f2764aba2eb80f3d65
大笨峰 <51231647+returndbf@users.noreply.github.com>
2025-01-20 04:47:27 +0000

    chore: add rolldown bundler

---
+++ rolldown.config.js
@@ -0,0 +1,29 @@
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import babel from '@rollup/plugin-babel';
+export default {
+    input: 'index.js', // 输入文件路径
+    output: [
+        {
+            file: 'dist/cjs/bundle.cjs', // 输出为 CommonJS 模块
+            format: 'cjs',
+            sourcemap: true,
+            inlineDynamicImports:true
+        },
+        {
+            file: 'dist/esm/bundle.esm.js', // 输出为 ES 模块
+            format: 'es',
+            sourcemap: true,
+            inlineDynamicImports:true
+        }
+    ],
+    plugins: [
+        resolve(), // 解析 node_modules 中的模块
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**', // 排除 node_modules 中的文件
+            presets: ['@babel/preset-env']
+        }),
+        terser() // 压缩代码
+    ]
+};
